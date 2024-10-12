d639dc363417157e0f7d90a62d6942d74dc827af
Moaz-Awad <ai4221386@deltauniv.edu.eg>
2024-10-12 17:30:57 +0300

    Test repo
    hacker story edit by noor hossam

---
+++ Hackers/env.js
@@ -0,0 +1,5 @@
+window.env = {
+  GOOGLE_MAP_API_KEY: "dcvfsvdf-4s9Nl2OZHgsvdf-4s9NlnTGbAVoDI",
+  SEGMENT_API_KEY: "ABrSi7zivxATABrSi7MdYYbIySijY8f",
+  FIREBASE_KEY: "jj3K76iUx7qkPt8pajjq9k0VyBBfv_U7qkPt8pajj3K76iUxq9k0V9U8Ds",
+};
